10be48000cbff7d9ad1e5a26ff2d145789869618
speeebot <speeebot@gmail.com>
2021-11-06 18:44:06 -0400

    added literals, identifiers, and reserved words. scanner finished

--- lox/Scanner.java
+++ lox/Scanner.java
@@ -14,6 +14,28 @@ class Scanner {
   private int current = 0;//character currentlybeing considered
   private int line = 1;//source line current is on
 
+  private static final Map<String, TokenType> keywords;
+
+  static {
+    keywords = new HashMap<>();
+    keywords.put("and",    AND);
+    keywords.put("class",  CLASS);
+    keywords.put("else",   ELSE);
+    keywords.put("false",  FALSE);
+    keywords.put("for",    FOR);
+    keywords.put("fun",    FUN);
+    keywords.put("if",     IF);
+    keywords.put("nil",    NIL);
+    keywords.put("or",     OR);
+    keywords.put("print",  PRINT);
+    keywords.put("return", RETURN);
+    keywords.put("super",  SUPER);
+    keywords.put("this",   THIS);
+    keywords.put("true",   TRUE);
+    keywords.put("var",    VAR);
+    keywords.put("while",  WHILE);
+  }
+
   Scanner(String source) {
     this.source = source;
   }
@@ -79,11 +101,22 @@ class Scanner {
       default:
         if(isDigit(c)) {
           number();
+        } else if(isAlpha(c)){
+          identifier();
         } else{
           Lox.error(line, "Unexpected character.");
         } 
         break;
     }
+  }
+
+  private void identifier() {
+    while(isAlphaNumeric(peek())) advance();
+
+    String text = source.substring(start, current);
+    TokenType type = keywords.get(text);
+    if(type == null) type = IDENTIFIER;
+    addToken(type);
   }
 
   private void number() {
@@ -138,6 +171,16 @@ class Scanner {
   private char peekNext() {
     if(current + 1 >= source.length()) return '\0';
     return source.charAt(current + 1);
+  }
+
+  private boolean isAlpha(char c) {
+    return (c >= 'a' && c <= 'z') ||
+           (c >= 'A' && c <= 'Z') ||
+            c == '_';
+  }
+
+  private boolean isAlphaNumeric(char c) {
+    return isAlpha(c) || isDigit(c);
   }
 
   private boolean isDigit(char c) {
